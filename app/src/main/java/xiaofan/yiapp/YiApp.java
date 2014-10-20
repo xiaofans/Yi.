b7ae996de276b064195ea7d7a4ebff0d2153666f
xiaofans <ttbzyzy@163.com>
2014-10-20 18:39:04 +0800

    remove unused file

--- app/src/main/java/xiaofan/yiapp/YiApp.java
+++ app/src/main/java/xiaofan/yiapp/YiApp.java
@@ -1,13 +1,23 @@
 package xiaofan.yiapp;
 
-import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by zhaoyu on 2014/10/9.
  */
-public class YiApp  extends Application{
+public class YiApp  extends MultiDexApplication{
 
     private static final String TAG = YiApp.class.getSimpleName();
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
 
     @Override
     public void onCreate() {
