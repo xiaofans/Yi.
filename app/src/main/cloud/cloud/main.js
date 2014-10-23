b768a8fc2b8c9e113c6bfce24b3f4f5e75770fd2
xiaofans <ttbzyzy@163.com>
2014-10-24 00:06:20 +0800

    issue clear

--- app/src/main/cloud/cloud/main.js
+++ app/src/main/cloud/cloud/main.js
@@ -1,6 +1,7 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+require("cloud/json3.js");
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
@@ -14,12 +15,12 @@ Parse.Cloud.define("signup", function(request, response) {
         id:123435,
         me:1
     };
-    response.success(request);
-   /* Parse.Cloud.httpRequest({
-      url: 'https://api.weibo.com/2/users/show.json',
+  var p = request.params.name_value_pairs;
+   Parse.Cloud.httpRequest({
+      url:'https://api.weibo.com/2/users/show.json',
       params: {
-      access_token:request.token,
-      uid:request.id,
+      access_token:p.token,
+      uid:p.id,
       source:"2718899853"
       },
       success: function(httpResponse) {
@@ -28,8 +29,8 @@ Parse.Cloud.define("signup", function(request, response) {
       },
       error: function(httpResponse) {
         console.error('Request failed with response code ' + httpResponse.status);
-        response.success("MY ERROR!"+httpResponse.text);
+        response.success("MY ERROR! token is;" + p.token +" "+httpResponse.text);
       }
-    });*/
+    });
 
 });
