60a1891dc0e3a6a9c16ffcb2bbfedb1c97e8b5d0
xiaofans <ttbzyzy@163.com>
2014-11-15 23:17:18 +0800

    add app icon

--- app/src/main/java/xiaofan/yiapp/ui/SettingsActivity.java
+++ app/src/main/java/xiaofan/yiapp/ui/SettingsActivity.java
@@ -19,6 +19,6 @@ public class SettingsActivity extends AuthenticatedActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
+        setContentView(R.layout.activity_about);
     }
 }
